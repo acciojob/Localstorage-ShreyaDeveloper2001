00677178057061e52f0d76433dd35a438a4ad05f
ShreyaDeveloper2001 <134867803+ShreyaDeveloper2001@users.noreply.github.com>
2023-09-10 23:27:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,47 @@
-{}
+
+	<script>
+        const addItemForm = document.getElementById('addItemForm');
+        const itemNameInput = document.getElementById('itemName');
+        const platesList = document.getElementById('platesList');
+        const items = JSON.parse(localStorage.getItem('items')) || [];
+
+        function addItem(e) {
+            e.preventDefault();
+
+            const itemName = itemNameInput.value;
+            if (!itemName) return;
+
+            const item = {
+                text: itemName,
+                done: false
+            };
+			items.push(item);
+            populateList(items, platesList);
+            localStorage.setItem('items', JSON.stringify(items));
+            itemNameInput.value = '';
+        }
+
+        function toggleDone(e) {
+            if (!e.target.matches('input')) return;
+
+            const index = e.target.dataset.index;
+            items[index].done = !items[index].done;
+            localStorage.setItem('items', JSON.stringify(items));
+            populateList(items, platesList);
+        }
+	function populateList(plates = [], platesList) {
+            platesList.innerHTML = plates.map((plate, i) => {
+                return `
+                <li>
+                    <input type="checkbox" data-index="${i}" id="item${i}" ${plate.done ? 'checked' : ''} />
+                    <label for="item${i}">${plate.text}</label>
+                </li>
+                `;
+            }).join('');
+        }
+
+        addItemForm.addEventListener('submit', addItem);
+        platesList.addEventListener('click', toggleDone);
+        populateList(items, platesList);
+    </script>
+
